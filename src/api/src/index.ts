774de00ce08bf93d31f50d31af021dae29d9be58
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2024-01-13 13:30:41 +0100

    testing deployment pipeline

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -8,7 +8,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+    res.send("Skog ❤️");
 });
 
 app.listen(port, () => {
