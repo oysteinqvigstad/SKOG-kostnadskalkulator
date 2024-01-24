5f74c61df60e381df471bf2f40908f9087ff1f70
andrrma <andrrma@stud.ntnu.no>
2024-01-24 14:10:59 +0100

    Fixes test for calculator function. Adds rounding of expected output due to rounding in the current implementation in Excel.

--- src/client/__tests__/calculator.test.ts
+++ src/client/__tests__/calculator.test.ts
@@ -1,11 +1,24 @@
 import {calculator, CalcResult} from "../src/calculator/calculator";
-// test("Should result in same result as excel-calculator",  () => {
-//     const result = new CalcResult(85,19.9)
-//
-//     expect(result).toEqual(calculator(1680,
-//         100,
-//         3,
-//         150,
-//         2,
-//         25))
-// })
+test("Should result in same result as excel-calculator",  () => {
+    const expectedCostPerTimberCubedRounded = 85;
+    const expectedTimberCubedPerG15HourRounded = 19.9;
+
+    const {costPerTimberCubed, timberCubedPerG15Hour} =calculator(
+        1680,
+        100,
+        3,
+        150,
+        2,
+        25
+    )
+
+    expect({
+        expectedCostPerTimberCubedRounded,
+        expectedTimberCubedPerG15HourRounded,
+    }).toEqual(
+        {
+            expectedCostPerTimberCubedRounded: Math.round(costPerTimberCubed),
+            expectedTimberCubedPerG15HourRounded: Number.parseFloat(timberCubedPerG15Hour.toFixed(1))
+        }
+    )
+})
--- src/client/src/calculator/calculator.ts
+++ src/client/src/calculator/calculator.ts
@@ -13,16 +13,37 @@ export function calculator(harvesterHourCostG15:number,
                            clearanceTrees1000SQM: number,
                            incline: number,
                            sellableTimberVolume: number) : CalcResult {
-    const middleStem = sellableTimberVolume / timberTrees1000SQM
-    const actualDrivingSpeedMetersPerMinute = 1 + 5 / clearanceTrees1000SQM - 0.1 * drivingConditions - 0.1 * incline
-    const t1Transport = 60 * (1000 / (13.3 * clearanceTrees1000SQM * (25.9 * actualDrivingSpeedMetersPerMinute)))
-    const t2FellingAndProcessing = 60 * (30.3 + 81.2 * middleStem)
+
+    const middleStem = sellableTimberVolume / timberTrees1000SQM;
+
+    // T1
+    const drivingSpeedReductionCoefficient = 1 + 5 / timberTrees1000SQM - 0.1 * drivingConditions - 0.1 * incline
+    const drivingSpeed = 25.9;
+    const t1Transport = 60 * (1000 / (13.3 * timberTrees1000SQM * (drivingSpeed * drivingSpeedReductionCoefficient)))
+
+    // T2
+    const t2FellingAndProcessing = 60 * ((30.3 + 81.2 * middleStem) / 100)
+
+    // T3
     const t3AddtionalTime = 1.5
-    const tR2ClearanceTrees = 1.8 * clearanceTrees1000SQM / timberTrees1000SQM
+
+    // TR2
+    const tR2ClearanceTrees = 1.8 * (clearanceTrees1000SQM / timberTrees1000SQM)
+
+
     const secondsPerTree = t1Transport + t2FellingAndProcessing + t3AddtionalTime + tR2ClearanceTrees
     const secondsPerTreeG15 = secondsPerTree * 1.2
-    const timberCubedPerG15Hour = 3600 / secondsPerTreeG15 / middleStem
-    const costPerTimberCubed = timberCubedPerG15Hour / harvesterHourCostG15
+
+
+    const timberCubedPerG15Hour = 3600 / (secondsPerTreeG15 / middleStem)
+    const costPerTimberCubed = harvesterHourCostG15 / timberCubedPerG15Hour
+
+    console.log("drivingSpeedReductionCoefficient",drivingSpeedReductionCoefficient)
+
+    console.log("t1Transport",t1Transport)
+
+
+
 
     return new CalcResult(costPerTimberCubed,timberCubedPerG15Hour)
 }
