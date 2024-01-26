de6a31f6511958f4840c52feaf37e66fcb712a55
andrrma <andrrma@stud.ntnu.no>
2024-01-26 14:17:17 +0100

    Adds calculator for load carrier with tests. Reorganizes parameters of both cost calculator functions into objects; RoadData and TreeData.

--- src/client/src/calculator/calculator-constants.ts
+++ src/client/src/calculator/calculator-constants.ts
@@ -1,6 +1,7 @@
 export const G0_TO_G15_FACTOR = 1.2
 export const NAIVE_DRIVING_SPEED = 25.9
 export const WORK_WIDTH = 13.3
-export const BASE_TIME = 30.3
-export const MIDDLESTEM_GROWTH_FACTOR = 81.2
 export const CLEARANCE_TIME_PER_TREE = 1.8
+export const DOUBLE_SAWING_TIME_G0_SECONDS = 28
+export const ROAD_OBSTRUCTIONS_TIME_G0_SECONDS = 15
+export const DIFFICULT_PROCESSING_TIME_G0_SECONDS = 37
