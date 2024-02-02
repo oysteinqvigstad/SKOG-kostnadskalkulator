088e9a8ad37dcc88dfc973c1244d77f9356f3601
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2024-02-02 15:10:23 +0100

    ekskluderer node_modules fra gcloud upload i håp om raskere deployment

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,14 +1,18 @@
 // src/index.js
 import express, { Express} from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname, '..', '..', '..', 'client', 'build')));
-// console.log(__dirname)
+
+// WARNING: Google App Engine has been configured to use a frontend/middleware handler for
+// the static files for faster processing. No request will actually reach the handler
+// that is defined below, on the production PaaS server.
+app.use(express.static(path.join(__dirname, '..', '..', '..', 'client', 'build')));
 
 
 app.listen(port, () => {
