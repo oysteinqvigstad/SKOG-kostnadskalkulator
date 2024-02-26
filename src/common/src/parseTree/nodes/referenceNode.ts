7f313b6e28353e2af863b729851b3e189ea52993
andrrma <andrrma@stud.ntnu.no>
2024-02-26 21:13:44 +0100

    Adds reference node type

---
+++ src/common/src/parseTree/nodes/referenceNode.ts
@@ -0,0 +1,10 @@
+import type {ParseNode} from "./parseNode";
+import {NodeType} from "../nodeMeta/node";
+
+export interface ReferenceNode extends ParseNode {
+    referenceID: string
+}
+
+export function isReferenceNode(node: ParseNode) : node is ReferenceNode {
+    return node.type === NodeType.Reference;
+}
