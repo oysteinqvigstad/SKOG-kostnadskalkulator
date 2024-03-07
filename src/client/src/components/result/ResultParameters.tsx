c031c89bd47595a4e0ecc2cd8bef1ce7ccc21a79
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2024-03-07 23:38:04 +0100

    fixed a bug related to toggle button not shown as enabled when clicked

--- src/client/src/components/result/ResultParameters.tsx
+++ src/client/src/components/result/ResultParameters.tsx
@@ -9,14 +9,25 @@ import {resetAllFields, toggleHideAdvanced} from "../../state/formSlice";
 
 export function ResultParameters() {
 
+    return (
+        <ResultCard
+            icon={<FcMultipleInputs />}
+            title={"Kostnadsdrivere"}
+            left={<SettingsPane />}
+        >
+            <ParametersWithTabs />
+        </ResultCard>
+    )
+}
+
+function SettingsPane() {
     const dispatch = useAppDispatch()
     const hideAdvanced = useAppSelector((state) => state.form.hideAdvanced)
 
-
-    const left =(
+    return (
         <Dropdown id={"field-settings"} autoClose={"outside"} align={{xs: 'start'}}>
             <Dropdown.Toggle>
-                <MdTune fontSize={'1.2em'} />
+                <MdTune fontSize={'1.2em'}/>
             </Dropdown.Toggle>
             <Dropdown.Menu>
                 <Dropdown.Item
@@ -25,27 +36,16 @@ export function ResultParameters() {
                     <MdReplay fontSize={'1.2em'} className={'ms-1 me-3'}/>
                     {"Nullstill alle felt"}
                 </Dropdown.Item>
-                <Dropdown.Item
-                    onClick={() => dispatch(toggleHideAdvanced())}
-                >
+                <Dropdown.Item as={"div"}>
                     <Form.Check
                         type={"switch"}
                         label={"Skjul avanserte felt"}
+                        id={"advanced-switch"}
                         checked={hideAdvanced}
+                        onChange={() => dispatch(toggleHideAdvanced())}
                     />
                 </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
-    )
-
-
-    return (
-        <ResultCard
-            icon={<FcMultipleInputs />}
-            title={"Kostnadsdrivere"}
-            left={left}
-        >
-            <ParametersWithTabs />
-        </ResultCard>
     )
 }
