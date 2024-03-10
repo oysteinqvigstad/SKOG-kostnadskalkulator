b1a7ba78d7d2738d414a799854d9276583957cca
Paul Schiager <paulschi@stud.ntnu.no>
2024-03-10 20:43:47 +0100

    Added simple preview-dropdown.

--- src/admin-client/src/rete/customControls/inputNodeControls/dropdown/dropdownInputControlContainer.tsx
+++ src/admin-client/src/rete/customControls/inputNodeControls/dropdown/dropdownInputControlContainer.tsx
@@ -4,10 +4,11 @@ import {TextInputField} from "../../../../components/input/textInputField";
 import {DropdownInputControl} from "./dropdownInputControl";
 import {Provider} from "react-redux";
 import {store} from "../../../../state/store";
-import {Col, Dropdown, DropdownButton, Row} from "react-bootstrap";
+import {Col, DropdownButton, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import {NumberInputField} from "../../../../components/input/numberInputField";
 import {InputGroup} from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
 
 
 export function DropdownInputControlContainer(
@@ -32,12 +33,15 @@ export function DropdownInputContolContent(
                 props.data.update();
             }}/>
 
-            <TextInputField value={props.data.name} inputHint={'Input Name'} onChange={
-                (value: string) => {
-                    props.data.name = value;
-                    props.data.update();
-                }
-            }/>
+            <TextInputField
+                value={props.data.name}
+                inputHint={'Input Name'}
+                onChange={
+                    (value: string) => {
+                        props.data.name = value;
+                        props.data.update();
+                    }
+                }/>
 
             <HiddenOnMinimized
                 minimized={props.data.options.minimized}
@@ -45,52 +49,71 @@ export function DropdownInputContolContent(
                     <>
                         <Row>
                             <Col>
-                                Hello world
-                            </Col>
-                        </Row>
-                        <Button
-                            onDoubleClick={(e) => {
-                                e.stopPropagation()
-                            }}
-                            onPointerDown={(e) => {
-                                e.stopPropagation()
-                            }}
-                            onClick={() => {
-                                props.data.dropdownOptions.push({
-                                    value: 0,
-                                    label: `option ${props.data.dropdownOptions.length + 1}`
-                                })
-                                props.data.update();
-                            }}
-                        >
-                            Add option
-                        </Button>
-                        {props.data.dropdownOptions.map((item, index) => {
-                            return <InputGroup className="mb-3">
 
-                                <TextInputField value={""} inputHint={"name"} onChange={() => {
-                                    return
-                                }}/>
-                                <NumberInputField
-                                    inputHint={"value"}
-                                    value={props.data.dropdownOptions[index].value}
-                                    onChange={() => {
-                                        return
-                                    }}
-                                    onIllegalValue={() => {
-                                        return
-                                    }}
-                                    legalRanges={[{max: 100}]}/>
                                 <Button
+                                    onDoubleClick={(e) => {
+                                        e.stopPropagation()
+                                    }}
+                                    onPointerDown={(e) => {
+                                        e.stopPropagation()
+                                    }}
                                     onClick={() => {
-                                        props.data.dropdownOptions.splice(index, 1);
+                                        props.data.dropdownOptions.push({
+                                            value: 0,
+                                            label: `option ${props.data.dropdownOptions.length + 1}`
+                                        })
                                         props.data.update();
                                     }}
-                                    onPointerDown={(e) => {e.stopPropagation()}}
-                                    onDoubleClick={(e) => {e.stopPropagation()}}
-                                >X</Button>
-                            </InputGroup>
-                        })}
+                                >
+                                    Add option
+                                </Button>
+                                {props.data.dropdownOptions.map((item, index) => {
+                                    return <InputGroup className="mb-3">
+
+                                        <TextInputField
+                                            value={item.label}
+                                            inputHint={"name"}
+                                            onChange={(newLabel) => {
+                                                props.data.dropdownOptions[index].label = newLabel
+                                                props.data.update();
+                                            }
+                                            }/>
+                                        <NumberInputField
+                                            inputHint={"value"}
+                                            value={props.data.dropdownOptions[index].value}
+                                            onChange={(value) => {
+                                                props.data.dropdownOptions[index].value = value;
+                                                props.data.update();
+                                            }}
+                                            onIllegalValue={() => {
+                                                return
+                                            }}
+                                        // TODO: Fjern magisk tall (lagt inn for testing)
+                                            legalRanges={[{max: 100}]
+                                        }/>
+                                        <Button
+                                            onClick={() => {
+                                                props.data.dropdownOptions.splice(index, 1);
+                                                props.data.update();
+                                            }}
+                                            onPointerDown={(e) => {
+                                                e.stopPropagation()
+                                            }}
+                                            onDoubleClick={(e) => {
+                                                e.stopPropagation()
+                                            }}
+                                        >X</Button>
+                                    </InputGroup>
+
+
+                                })}
+                                <DropdownButton title={'Preview'} id={'preview'}>
+                                    {props.data.dropdownOptions.map((item) => {
+                                        return <Dropdown.Item>{item.label}</Dropdown.Item>
+                                        })}
+                                </DropdownButton>
+                            </Col>
+                        </Row>
                     </>
                 }
             />
