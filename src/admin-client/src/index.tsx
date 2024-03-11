b697192d696d0eed2ca9eb9f38bdad79d9289e87
andrrma <andrrma@stud.ntnu.no>
2024-03-11 21:22:50 +0100

    Adds Provider to index.tsx

--- src/admin-client/src/index.tsx
+++ src/admin-client/src/index.tsx
@@ -1,16 +1,20 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import App from "./App";
+import {Provider} from "react-redux";
+import {store} from "./state/store";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+    <Provider store={store}>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </Provider>
 );
