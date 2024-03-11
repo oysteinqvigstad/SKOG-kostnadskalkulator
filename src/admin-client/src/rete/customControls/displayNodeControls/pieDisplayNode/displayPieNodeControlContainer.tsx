9fd9f942dbeb966be1d1164931700a18deadb757
andrrma <andrrma@stud.ntnu.no>
2024-03-11 20:16:49 +0100

    Adds Provider context for access to store

--- src/admin-client/src/rete/customControls/displayNodeControls/pieDisplayNode/displayPieNodeControlContainer.tsx
+++ src/admin-client/src/rete/customControls/displayNodeControls/pieDisplayNode/displayPieNodeControlContainer.tsx
@@ -3,30 +3,39 @@ import React from "react";
 import {ResultPie} from "../../../../sharedWithClient/resultPie";
 import {NodeType} from "@skogkalk/common/dist/src/parseTree";
 import {useAppSelector} from "../../../../state/hooks";
-import {selectTreeState} from "../../../../state/store";
+import {selectTreeState, store} from "../../../../state/store";
 import {NodeControl} from "../../../nodes/baseNode";
+import {Provider} from "react-redux";
 
 
 export function DisplayPieNodeControlContainer(
     props: { data: NodeControl<DisplayPieNodeData> }
 ) {
+    return <Provider store={store}>
+        <DisplayPieNodeContent data={props.data}/>
+    </Provider>
+}
+
+function DisplayPieNodeContent(
+    props: { data: NodeControl<DisplayPieNodeData>}
+) {
     const treeState = useAppSelector(selectTreeState)
 
     return <>
-    <ResultPie
-        displayData={{
-            id:"",
-            name: props.data.data.name,
-            inputs: [
+        <ResultPie
+            displayData={{
+                id:"",
+                name: props.data.data.name,
+                inputs: [
 
-            ],
-            unit: "",
-            pieType: "donut",
-            inputOrdering: [],
-            type: NodeType.Display,
-            value: 0
-        }}
-        treeState={treeState.tree}
-    />
+                ],
+                unit: "",
+                pieType: "donut",
+                inputOrdering: [],
+                type: NodeType.Display,
+                value: 0
+            }}
+            treeState={treeState.tree}
+        />
     </>
 }
