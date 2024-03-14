11a6e37041fa52ef97030c1b4281933899daa688
Paul Schiager <paulschi@stud.ntnu.no>
2024-03-14 15:10:45 +0100

    Added basic input validation, WIP.

--- src/admin-client/src/rete/customControls/inputNodeControls/dropdown/dropdownInputControlContainer.tsx
+++ src/admin-client/src/rete/customControls/inputNodeControls/dropdown/dropdownInputControlContainer.tsx
@@ -14,6 +14,7 @@ import {OptionSwitch} from "../../../../components/input/optionSwitch";
 import {useAppSelector} from "../../../../state/hooks";
 
 
+
 export function DropdownInputControlContainer(
     props: { data: NodeControl<DropdownInputControlData> }
 ) {
@@ -60,10 +61,11 @@ export function DropdownInputControlContent(
                                         )}
                                     onChange={(index) => {
                                         props.data.set({
-                                            defaultKey: data.dropdownOptions[index].label,
-                                            defaultValue: data.dropdownOptions[index].value}
+                                                defaultKey: data.dropdownOptions[index].label,
+                                                defaultValue: data.dropdownOptions[index].value
+                                            }
                                         )
-                                        
+
                                         console.log(data.defaultKey)
                                     }}
                                 />
@@ -72,87 +74,96 @@ export function DropdownInputControlContent(
                                 <OptionSwitch on={data.simpleInput} onChange={
                                     (on: boolean) => {
                                         props.data.set({simpleInput: on});
-                                        
+
                                     }
-                                } />
+                                }/>
                             </Col>
                         </Row>
+                        <Button
+                            onDoubleClick={(e) => {
+                                e.stopPropagation()
+                            }}
+                            onPointerDown={(e) => {
+                                e.stopPropagation()
+                            }}
+                            onClick={() => {
+                                data.dropdownOptions.push({
+                                    value: 0,
+                                    label: `option ${data.dropdownOptions.length + 1}`
+                                })
+                                props.data.set({dropdownOptions: data.dropdownOptions})
+                            }}
+                        >
+                            Add option
+                        </Button>
+                        {data.dropdownOptions.map((item, index) => {
+                            return <InputGroup key={item.label} className="mb-3">
+
+                                <TextInputField
+                                    value={item.label}
+                                    inputHint={"name"}
+                                    isValid={(newLabel) =>
+                                        // compare new item label to all others:
+                                        data.dropdownOptions.filter((filteredItem) =>
+                                            filteredItem !== item).every((entry) =>
+                                            newLabel !== entry.label)}
+                                    // TODO: Endre validering så oppdatering bare kan skje med valid navn?
+                                    onChange={(newLabel) => {
+                                        data.dropdownOptions[index].label = newLabel
+                                        props.data.set({dropdownOptions: data.dropdownOptions})
+                                        // TODO: Må endre tegn for tegn om denne er med:
+                                        //props.data.update();
+                                    }}
+                                />
+                                <NumberInputField
+                                    inputHint={"value"}
+                                    value={data.dropdownOptions[index].value}
+                                    onChange={(value) => {
+                                        data.dropdownOptions[index].value = value;
+                                        props.data.set({dropdownOptions: data.dropdownOptions})
+                                    }}
+                                    onIllegalValue={() => {
+                                    }}
+                                    // TODO: Fjern magisk tall (lagt inn for testing)
+                                    legalRanges={[{max: 100}]
+                                    }/>
                                 <Button
-                                    onDoubleClick={(e) => {
-                                        e.stopPropagation()
+                                    onClick={() => {
+                                        data.dropdownOptions.splice(index, 1);
+                                        props.data.update();
                                     }}
                                     onPointerDown={(e) => {
                                         e.stopPropagation()
                                     }}
-                                    onClick={() => {
-                                        data.dropdownOptions.push({
-                                            value: 0,
-                                            label: `option ${data.dropdownOptions.length + 1}`
-                                        })
-                                        props.data.set({dropdownOptions: data.dropdownOptions})
+                                    onDoubleClick={(e) => {
+                                        e.stopPropagation()
                                     }}
-                                >
-                                    Add option
-                                </Button>
-                                {data.dropdownOptions.map((item, index) => {
-                                    return <InputGroup key={item.label} className="mb-3">
-
-                                        <TextInputField
-                                            value={item.label}
-                                            inputHint={"name"}
-                                            onChange={(newLabel) => {
-                                                data.dropdownOptions[index].label = newLabel
-                                                props.data.set({dropdownOptions: data.dropdownOptions})
-                                                // TODO: Må endre tegn for tegn om denne er med:
-                                                //props.data.update();
-                                            }}
-                                        />
-                                        <NumberInputField
-                                            inputHint={"value"}
-                                            value={data.dropdownOptions[index].value}
-                                            onChange={(value) => {
-                                                data.dropdownOptions[index].value = value;
-                                                props.data.set({dropdownOptions: data.dropdownOptions})
-                                            }}
-                                            onIllegalValue={() => {}}
-                                            // TODO: Fjern magisk tall (lagt inn for testing)
-                                            legalRanges={[{max: 100}]
-                                            }/>
-                                        <Button
-                                            onClick={() => {
-                                                data.dropdownOptions.splice(index, 1);
-                                                props.data.update();
-                                            }}
-                                            onPointerDown={(e) => {
-                                                e.stopPropagation()
-                                            }}
-                                            onDoubleClick={(e) => {
-                                                e.stopPropagation()
-                                            }}
-                                        >X</Button>
-                                        <Button onClick={() => {
-                                            if (index >= 0 && index < data.dropdownOptions.length - 1) {
-                                                [data.dropdownOptions[index], data.dropdownOptions[index + 1]] =
-                                                    [data.dropdownOptions[index + 1], data.dropdownOptions[index]]
-                                                props.data.set({dropdownOptions: data.dropdownOptions})
-                                            }
-                                            props.data.update();
-                                        }}>Down</Button>
-                                        <Button onClick={() => {
-                                            if (index > 0) {
-                                                [data.dropdownOptions[index], data.dropdownOptions[index - 1]] =
-                                                    [data.dropdownOptions[index - 1], data.dropdownOptions[index]]
-                                                props.data.set({dropdownOptions: data.dropdownOptions})
-                                            }
-                                        }}>Up</Button>
-                                    </InputGroup>
-                                })}
+                                >X</Button>
+                                <Button onClick={() => {
+                                    if (index >= 0 && index < data.dropdownOptions.length - 1) {
+                                        [data.dropdownOptions[index], data.dropdownOptions[index + 1]] =
+                                            [data.dropdownOptions[index + 1], data.dropdownOptions[index]]
+                                        props.data.set({dropdownOptions: data.dropdownOptions})
+                                    }
+                                    props.data.update();
+                                }}>Down</Button>
+                                <Button onClick={() => {
+                                    if (index > 0) {
+                                        [data.dropdownOptions[index], data.dropdownOptions[index - 1]] =
+                                            [data.dropdownOptions[index - 1], data.dropdownOptions[index]]
+                                        props.data.set({dropdownOptions: data.dropdownOptions})
+                                    }
+                                }}>Up</Button>
+                            </InputGroup>
+                        })}
                         <DropdownSelection
                             inputHint={"Select page"}
                             selection={pages.find((page) => page.title === data.pageName)?.ordering}
-                            dropdownAlternatives={pages.map((page) => {return {label: page.title, value: page.ordering}})}
+                            dropdownAlternatives={pages.map((page) => {
+                                return {label: page.title, value: page.ordering}
+                            })}
                             onChange={(selected: number) => {
-                                props.data.set ({pageName: pages.find((page) => page.ordering === selected)?.title});
+                                props.data.set({pageName: pages.find((page) => page.ordering === selected)?.title});
                                 props.data.update()
                             }}
                         />
