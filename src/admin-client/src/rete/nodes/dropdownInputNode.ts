5fcf9a30d8120e2b869df1b2d577ba7edb175670
Paul Schiager <paulschi@stud.ntnu.no>
2024-03-14 10:41:20 +0100

    Added vertical sizing of dropdown input.

--- src/admin-client/src/rete/nodes/dropdownInputNode.ts
+++ src/admin-client/src/rete/nodes/dropdownInputNode.ts
@@ -41,7 +41,7 @@ export class DropdownInputNode extends BaseNode<
                         this.height = this.originalHeight * 0.5;
                     } else {
                         this.width = this.originalWidth;
-                        this.height = this.originalHeight;
+                        this.height = this.originalHeight + this.controls.c.get('dropdownOptions').length * 60;
                     }
                     this.updateNodeRendering(this.id);
                     this.updateDataFlow();
