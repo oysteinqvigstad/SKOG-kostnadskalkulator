c148cb12bb17a1f230bc7e327628d6aac851cb24
Paul Schiager <paulschi@stud.ntnu.no>
2024-03-18 15:06:45 +0100

    Input ordering per page, WIP. Bug: Buttons not always working.

--- src/admin-client/src/state/slices/pages.ts
+++ src/admin-client/src/state/slices/pages.ts
@@ -8,12 +8,12 @@ export interface Page {
 }
 
 export interface PagesState {
-    pages: {id: number, page:Page}[];
+    pages: { id: number, page: Page }[];
     selected: number | undefined;
 }
 
-function updateOrdering(pagesWithID: {id: number, page: Page}[]) {
-    pagesWithID.forEach(( {id, page}, index) => {
+function updateOrdering(pagesWithID: { id: number, page: Page }[]) {
+    pagesWithID.forEach(({id, page}, index) => {
         page.ordering = index;
     });
 }
@@ -29,9 +29,9 @@ export const pageSlice = createSlice({
         setPagesState: (state, action: PayloadAction<PagesState>) => {
             return action.payload;
         },
-        addPage: (state, action: PayloadAction<Page> ) => {
+        addPage: (state, action: PayloadAction<Page>) => {
             action.payload.ordering = state.pages.length;
-            state.pages.push({id: ( Math.random() * 1000000 + 1000000), page: action.payload});
+            state.pages.push({id: (Math.random() * 1000000 + 1000000), page: action.payload});
         },
         removePage: (state, action: PayloadAction<number>) => {
             const tail = state.pages.slice(action.payload + 1);
@@ -42,23 +42,31 @@ export const pageSlice = createSlice({
         updatePage: (state, action: PayloadAction<Page>) => {
             state.pages[action.payload.ordering].page = action.payload;
         },
-        movePage: (state, action: PayloadAction<{oldIndex: number, newIndex: number}>) => {
+        movePage: (state, action: PayloadAction<{ oldIndex: number, newIndex: number }>) => {
             const {oldIndex, newIndex} = action.payload;
-            if(newIndex < 0 || newIndex >= state.pages.length) return;
+            if (newIndex < 0 || newIndex >= state.pages.length) return;
             [state.pages[oldIndex], state.pages[newIndex]] =
                 [state.pages[newIndex], state.pages[oldIndex]];
             updateOrdering(state.pages);
         },
         addInputToPage: (state, action: PayloadAction<{ nodeID: string, pageName: string }>) => {
-            state.pages.forEach(({ page })=>{
-                page.inputIds = page.inputIds.filter(id=>id !== action.payload.nodeID)
-                if(page.title === action.payload.pageName) {
+            state.pages.forEach(({page}) => {
+                page.inputIds = page.inputIds.filter(id => id !== action.payload.nodeID)
+                if (page.title === action.payload.pageName) {
                     page.inputIds.push(action.payload.nodeID);
                 }
             })
-            console.log(state.pages.map(({page})=> {
-                return { page: (page.title), inputs: (page.inputIds)}
+            console.log(state.pages.map(({page}) => {
+                return {page: (page.title), inputs: (page.inputIds)}
             }))
+        },
+        moveInput: (state, action: PayloadAction<{ oldIndex: number, newIndex: number }>) => {
+            const {oldIndex, newIndex} = action.payload;
+            state.pages.map(({page})=> {
+                if(newIndex < 0 || newIndex >= page.inputIds.length) return;
+                    [page.inputIds[newIndex], page.inputIds[oldIndex]] =
+                    [page.inputIds[oldIndex], page.inputIds[newIndex]];
+            })
         },
         setPageSelection: (state, action: PayloadAction<number>) => {
             state.selected = action.payload;
@@ -66,5 +74,14 @@ export const pageSlice = createSlice({
     }
 });
 
-export const {addPage, removePage, updatePage, movePage, addInputToPage, setPageSelection, setPagesState} = pageSlice.actions;
+export const {
+    addPage,
+    removePage,
+    updatePage,
+    movePage,
+    addInputToPage,
+    moveInput,
+    setPageSelection,
+    setPagesState
+} = pageSlice.actions;
 export const pagesReducer = pageSlice.reducer;
