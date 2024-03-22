7bbdf5e1ddd4e5d67a96aea14d678ab4f69548a3
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2024-03-22 23:45:34 +0100

    show visuals from display nodes in client

--- src/client/src/containers/ResultContent.tsx
+++ src/client/src/containers/ResultContent.tsx
@@ -1,28 +1,18 @@
 import React from "react";
 import {Col, Row, Stack} from "react-bootstrap";
 import {ResultParameters} from "../components/result/ResultParameters";
-import {ResultCard} from "../components/result/ResultCard";
-import {FcBullish, FcFinePrint, FcIdea, FcSalesPerformance} from "react-icons/fc";
-
+import {useAppSelector} from "../state/hooks";
+import {selectDisplayNodes, selectTreeState} from "../state/treeSelectors";
+import {DisplayBarNode, DisplayNode, DisplayPieNode, NodeType} from "@skogkalk/common/dist/src/parseTree";
+import {ResultBar} from "@skogkalk/common/dist/src/visual/resultBar";
+import {ResultPie} from "@skogkalk/common/dist/src/visual/resultPie";
 
 
 /**
- * Result page for the harvester and load carrier
+ * Render the panel for parameters and the results derived from the display nodes
  */
 export function ResultContent() {
-
-
-
-
-    // TODO: remove after testing
-    // let pieData: DisplayPieNode | undefined
-    // let treeCopy: TreeState | undefined
-    // if (treeState?.displayNodes[0]) {
-    //     treeCopy = treeStateFromData(treeState.subTrees)
-    //     pieData = {...treeState.displayNodes[0], unit: "", pieType: "pie"}
-    //
-    // }
-
+    const displayNodes = useAppSelector(selectDisplayNodes)
 
     return (
             <Stack className={"mb-3"} gap={3}>
@@ -30,35 +20,36 @@ export function ResultContent() {
                     <Col md={{span: 12, order: 1}} lg={{span: 8, order: 1}} className={"d-none d-md-block"}>
                         <ResultParameters />
                     </Col>
-                    <Col xs={{span: 12, order: 4}} md={{span: 6, order: 4}} lg={{span: 4, order: 2}}>
-                        <ResultCard
-                            icon={<FcIdea/>}
-                            title={"Innsikt i kostnadsdrivere"}
-                            children={<></>}
-                        />
-                    </Col>
-                    <Col xs={{span: 12, order: 1}} md={{span: 6, order: 2}} lg={{span: 4, order: 3}}>
-                         <ResultCard
-                             icon={<FcBullish />}
-                             title={"Produktivitet"}
-                             children={<></>}
-                         />
-                    </Col>
-                    <Col xs={{span: 12, order: 2}} md={{span: 6, order: 3}} lg={{span: 4, order: 4}}>
-                        <ResultCard
-                            icon={<FcSalesPerformance />}
-                            title={"Kostnad"}
-                            children={<></>}
-                        />
-                    </Col>
-                    <Col xs={{span: 12, order: 3}} md={{span: 6, order: 5}} lg={{span: 4, order: 5}}>
-                        <ResultCard
-                            icon={<FcFinePrint/>}
-                            title={"Kostnadsoverslag"}
-                            children={<></>}
-                        />
-                    </Col>
+                    {displayNodes?.map((node) => <ResultComponent displayNode={node} />)}
                 </Row>
             </Stack>
+    )
+}
+
+
+/**
+ * Render the result component based on the display node type
+ */
+function ResultComponent({displayNode}: {displayNode: DisplayNode}) {
+    const treeState = useAppSelector(selectTreeState)
+
+    /**
+     * Determine the component to render based on the display node type
+     */
+    const component = (node: DisplayNode) => {
+        switch (node.type) {
+            case NodeType.BarDisplay:
+                return <ResultBar displayData={node as DisplayBarNode} treeState={treeState} />
+            case NodeType.Display:
+                return <ResultPie displayData={node as DisplayPieNode} treeState={treeState} />
+            default:
+                return <></>
+        }
+    }
+
+    return (
+        <Col xs={{span: 12, order: 2}} md={{span: 6, order: 2}} lg={{span: 4, order: 2}}>
+            {component(displayNode)}
+        </Col>
     )
 }
