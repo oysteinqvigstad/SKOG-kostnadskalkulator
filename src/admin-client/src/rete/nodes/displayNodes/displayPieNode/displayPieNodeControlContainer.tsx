c12d9bd15b7359c66e2c6ff86b749186a0f5ab03
Paul Schiager <paulschi@stud.ntnu.no>
2024-04-05 13:57:08 +0200

    Added text editor to pie display node.

--- src/admin-client/src/rete/nodes/displayNodes/displayPieNode/displayPieNodeControlContainer.tsx
+++ src/admin-client/src/rete/nodes/displayNodes/displayPieNode/displayPieNodeControlContainer.tsx
@@ -10,8 +10,10 @@ import {OutputNode as ParseOutputNode} from "@skogkalk/common/dist/src/parseTree
 import {ResultPie} from "@skogkalk/common/dist/src/visual/resultPie";
 import Container from "react-bootstrap/Container";
 import {TextInputField} from "../../../../components/input/textInputField";
-import { DisplayPieNode as ParseDisplayNode} from "@skogkalk/common/dist/src/parseTree"
+import {DisplayPieNode as ParseDisplayNode} from "@skogkalk/common/dist/src/parseTree"
 import {NodeControl} from "../../nodeControl";
+import {TextEditor} from "../../../../components/input/textEditor";
+import { Drag } from "rete-react-plugin";
 
 export function DisplayPieNodeControlContainer(
     props: { data: NodeControl<DisplayPieNodeData> }
@@ -22,47 +24,55 @@ export function DisplayPieNodeControlContainer(
 }
 
 function DisplayPieNodeContent(
-    props: { data: NodeControl<DisplayPieNodeData>}
+    props: { data: NodeControl<DisplayPieNodeData> }
 ) {
     const treeState = useAppSelector(selectTreeState)
     const nodeID = props.data.get('nodeID');
     const [displayNode, setDisplayNode] = useState(getNodeByID(treeState.tree, nodeID) as ParseDisplayNode | undefined);
-    useEffect(()=> {
-        if(treeState.tree) {
+    useEffect(() => {
+        if (treeState.tree) {
             setDisplayNode(getNodeByID(treeState.tree, props.data.get('nodeID')) as ParseDisplayNode);
         }
     }, [treeState.tree, nodeID, displayNode])
 
 
     return <>
-        <Container>
-            <ResultPie
-                displayData={displayNode? displayNode : {
-                    id:"",
-                    value: 0,
-                    type: NodeType.Display,
-                    inputOrdering: [],
-                    name: "",
-                    unit: "",
-                    pieType: "donut",
-                    inputs:[],
+        <Drag.NoDrag>
+            <Container>
+                <ResultPie
+                    displayData={displayNode ? displayNode : {
+                        id: "",
+                        value: 0,
+                        type: NodeType.Display,
+                        inputOrdering: [],
+                        name: "",
+                        unit: "",
+                        pieType: "donut",
+                        inputs: [],
+                        infoText: ""
 
-                }}
-                treeState={treeState.tree}
-            />
-            <TextInputField
-                inputHint={"Name"}
-                value={props.data.get('name')}
-                onChange={(value)=>{
-                    props.data.set({name: value});
-                }}/>
-            <TextInputField
-                inputHint={"Unit"}
-                value={props.data.get('unit')}
-                onChange={(value)=>{
-                    props.data.set({unit: value});
-                }}/>
-        </Container>
+                    }}
+                    treeState={treeState.tree}
+                />
+                <TextInputField
+                    inputHint={"Name"}
+                    value={props.data.get('name')}
+                    onChange={(value) => {
+                        props.data.set({name: value});
+                    }}/>
+                <TextInputField
+                    inputHint={"Unit"}
+                    value={props.data.get('unit')}
+                    onChange={(value) => {
+                        props.data.set({unit: value});
+                    }}/>
+                <TextEditor
+                    value={props.data.get("infoText") || ""}
+                    onSave={(value: string) => {
+                        props.data.set({infoText: value});
+                    }}/>
 
+            </Container>
+        </Drag.NoDrag>
     </>
 }
