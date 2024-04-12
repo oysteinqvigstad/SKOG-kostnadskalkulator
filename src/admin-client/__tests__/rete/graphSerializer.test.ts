32c091aa60b8a60bc5150fcabcb41dd31de0bfd3
andrrma <andrrma@stud.ntnu.no>
2024-04-12 22:44:26 +0200

    Adds further assertions

--- src/admin-client/__tests__/rete/graphSerializer.test.ts
+++ src/admin-client/__tests__/rete/graphSerializer.test.ts
@@ -8,13 +8,14 @@ describe('GraphSerializer', ()=>{
 
         const serializer = new GraphSerializer(editor, factory);
         const moduleData = moduleManager.getModuleData(ModuleName.passThroughModule);
-        await serializer.importNodes(moduleData);
+        expect(moduleData).toBeDefined();
+        await serializer.importNodes(moduleData!);
         const serialized = serializer.exportNodes();
         expect(serialized.nodes.map(node=>node.id)).toEqual(moduleData?.nodes.map(node=>node.id));
 
         await editor.clear();
-
-        await serializer.importNodes(moduleData, true);
+        expect(moduleData).toBeDefined();
+        await serializer.importNodes(moduleData!, true);
         const newSerialized = serializer.exportNodes();
 
         expect(newSerialized.nodes.map(node=>node.id)).not.toEqual(serialized.nodes.map(node=>node.id));
