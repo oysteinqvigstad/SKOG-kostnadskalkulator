897ef5e9246c668f3e6fc8bebcff0a1cc058789b
andrrma <andrrma@stud.ntnu.no>
2024-04-12 22:43:21 +0200

    Refactors modulePanel to use new callback system from Editor

--- src/admin-client/src/containers/panels/modulePanel.tsx
+++ src/admin-client/src/containers/panels/modulePanel.tsx
@@ -1,28 +1,67 @@
-import {Editor} from "../../rete/editor";
-import {Button} from "react-bootstrap";
-import {useState} from "react";
+import {Editor, EditorEvent} from "../../rete/editor";
+import {Button, Modal} from "react-bootstrap";
+import {useState, useSyncExternalStore} from "react";
+import {ModuleEntry} from "../../rete/moduleManager";
+import {SingleFileUploader} from "../../components/filePicker";
 
 
 export function ModulePanel( props: { editor: Editor | undefined }) {
-    const [modules, setModules] = useState<string[]>(props.editor?.getModuleNames() || []);
-    const [selected, setSelected] = useState("main")
+    return <>
+        {props.editor && <ModulePanelContents editor={props.editor!}/>}
+    </>
+}
+
+const downloadModule = (filename: string, data: ModuleEntry) => {
+
+    const name = window.prompt("Export as", filename)
+    if(!name) {
+        return;
+    }
+    data.name = name!;
+    const json = JSON.stringify(data, null, 2);
+    const blob = new Blob([json], { type: "application/json" });
+    const href = URL.createObjectURL(blob);
+
+    // create "a" HTLM element with href to file
+    const link = document.createElement("a");
+    link.href = href;
+    link.download = name + ".json";
+    document.body.appendChild(link);
+    link.click();
+
+    // clean up "a" element & remove ObjectURL
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+}
+
+
+function ModulePanelContents( props: { editor: Editor }) {
+
+    let moduleSnapshot = useSyncExternalStore(
+        props.editor.createEventSubscriber(EditorEvent.ModulesChanged),
+        props.editor.getSnapshotRetriever()
+    );
+
+    const [showUploadModal, setShowUploadModal] = useState(false);
+
+    const mainLoaded = () => { return !props?.editor.hasModuleLoaded() }
+    const moduleLoaded = (name: string) => { return props.editor?.currentModule === name}
+
 
     return <>
         <Button
-            disabled={selected === 'main'}
+            disabled={!props.editor?.hasModuleLoaded()}
             onClick={ ()=>{
                 props.editor?.loadMainGraph();
-                setSelected('main');
             }}
         >Main</Button>
 
-        {modules.map((name)=>{
+        {moduleSnapshot.moduleNames.map((name)=>{
             return <Button
                 key={name}
-                disabled={selected === name}
+                disabled={moduleSnapshot.currentModule === name}
                 onClick={()=>{
                     props.editor?.loadModule(name);
-                    setSelected(name);
                 }}
             >{name}</Button>
         })}
@@ -31,11 +70,52 @@ export function ModulePanel( props: { editor: Editor | undefined }) {
                 const name = prompt("Module name");
                 if(name) {
                     props.editor?.addNewModule(name);
-                    setModules(props.editor?.getModuleNames || []);
-                    setSelected(name);
                 }
             }}>new</Button>
-            <Button onClick={()=>{props.editor?.saveCurrentModule()}}>save</Button>
+            <Button disabled={mainLoaded()} onClick={()=>{
+                props.editor.deleteModule(props.editor.currentModule!);
+            }}>delete</Button>
+            <Button disabled={mainLoaded()} onClick={()=>{
+                const newName = prompt("New name");
+                if(newName === "" || newName === 'main' || newName === null) return;
+                props.editor?.renameCurrentModule(newName);
+            }}>rename</Button>
+            <Button
+                disabled={mainLoaded()}
+                onClick={()=>{
+                    props.editor?.exportCurrentGraph()
+                        .then((data)=>{
+                            console.log("exporting", data);
+                            downloadModule(moduleSnapshot.currentModule!,
+                                { name: moduleSnapshot.currentModule!, data: data});
+                        });
+                }}
+            >Export</Button>
+            <Button
+                onClick={()=>{setShowUploadModal(true)}}
+            >import</Button>
+            <Modal
+                show={showUploadModal}
+            >
+                <SingleFileUploader handleFile={(file)=>{
+                    if(file && file.name.endsWith(".json")) {
+                        file.text()
+                            .then( (contents)=> {
+                                const data = JSON.parse(contents);
+                                console.log(data);
+                                if(data.data && data.name) {
+                                    props.editor?.addNewModule(data.name, data.data);
+                                }
+                            })
+                            .catch((
+
+                            )=>{})
+                    }
+                    setShowUploadModal(false);
+                }}
+                    abort={()=>{setShowUploadModal(false)}}
+                />
+            </Modal>
         </div>
-        </>
+    </>
 }
