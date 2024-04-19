914121e111a4269a13ff0d8ad8bba7359fef5ea0
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2024-04-20 00:27:40 +0200

    merged reset button (currently doesnt do anything)

--- src/client/src/components/result/ResultParameters.tsx
+++ src/client/src/components/result/ResultParameters.tsx
@@ -2,10 +2,8 @@ import {ResultCard} from "./ResultCard";
 import {FcMultipleInputs} from "react-icons/fc";
 import React from "react";
 import {ParametersWithTabs} from "../ParametersWithTabs";
-import {Dropdown, Form} from "react-bootstrap";
+import {Dropdown} from "react-bootstrap";
 import {MdReplay, MdTune} from "react-icons/md";
-import {useAppDispatch, useAppSelector} from "../../state/hooks";
-import {resetAllFields, toggleHideAdvanced} from "../../state/formSlice";
 
 export function ResultParameters() {
 
@@ -22,8 +20,6 @@ export function ResultParameters() {
 
 
 function SettingsPane() {
-    const dispatch = useAppDispatch()
-    const hideAdvanced = useAppSelector((state) => state.form.hideAdvanced)
 
     return (
         <Dropdown id={"field-settings"} autoClose={"outside"} align={{xs: 'start'}}>
@@ -32,19 +28,10 @@ function SettingsPane() {
             </Dropdown.Toggle>
             <Dropdown.Menu>
                 <Dropdown.Item
-                    onClick={() => dispatch(resetAllFields())}
+                    onClick={() => {}}
                 >
                     <MdReplay fontSize={'1.2em'} className={'ms-1 me-3'}/>
                     {"Tilbakestill alle felt"}
-                </Dropdown.Item>
-                <Dropdown.Item as={"div"}>
-                    <Form.Check
-                        type={"switch"}
-                        label={"Skjul avanserte felt"}
-                        id={"advanced-switch"}
-                        checked={hideAdvanced}
-                        onChange={() => dispatch(toggleHideAdvanced())}
-                    />
                 </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
