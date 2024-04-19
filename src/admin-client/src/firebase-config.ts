fa1be2aa598a6ded82ee81c1f6c2a0364e4fd410
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2024-04-19 20:11:00 +0200

    updated firebase auth config object for migration

--- src/admin-client/src/firebase-config.ts
+++ src/admin-client/src/firebase-config.ts
@@ -4,14 +4,13 @@ import { getAuth } from 'firebase/auth'
 const firebaseConfig = {
     // TODO: apiKey is not an access key, but they can be misused for bruteforcing attacks, etc.
     //  so we should apply stricter security rules as well
-    apiKey: "AIzaSyDqqH0vgnbhXK7t8ia7um9Tx3TfLwkCAcg",
-    authDomain: "modern-heading-411109.firebaseapp.com",
-    projectId: "modern-heading-411109",
-    storageBucket: "modern-heading-411109.appspot.com",
-    messagingSenderId: "64157141840",
-    appId: "1:64157141840:web:2a6f5b84a0c412d23c7a55",
-    measurementId: "G-7N0MMDJ14J"
-}
+        apiKey: "AIzaSyAFSvhdW12OcOM6h7P4sfJAkn6c1YRQvWc",
+        authDomain: "kostnadskalkulator.firebaseapp.com",
+        projectId: "kostnadskalkulator",
+        storageBucket: "kostnadskalkulator.appspot.com",
+        messagingSenderId: "428871549832",
+        appId: "1:428871549832:web:2f5aba37a9732921b2f03d",
+    };
 
 export const actionCodeSettings = {
     url: 'http://localhost:3000/access/confirm',
