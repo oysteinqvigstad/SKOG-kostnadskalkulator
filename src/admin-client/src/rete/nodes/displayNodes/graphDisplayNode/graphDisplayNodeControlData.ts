3aeeb003f6b0429661743d4746f3562065ca0426
andrrma <andrrma@stud.ntnu.no>
2024-04-21 20:33:02 +0200

    Adds control data interfaces for graphDisplayNode controllers

--- src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNodeControlData.ts
+++ src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNodeControlData.ts
@@ -1,5 +1,18 @@
 import {DisplayControlData} from "../displayNodeControlData";
+import {ResultGroup} from "@skogkalk/common/dist/src/parseTree/nodes/displayNode";
+
+
 
 export interface GraphDisplayNodeControlData extends DisplayControlData {
+    shouldAddGroup: boolean,
+    resultGrouping: ResultGroup[]
+    inputFieldShow: {name: string, page: string, id: string, show: boolean}[]
+}
 
+export interface GraphDisplayGroupData {
+    id: string,
+    name: string,
+    shouldDelete: boolean,
+    unit: string,
+    label: string
 }
