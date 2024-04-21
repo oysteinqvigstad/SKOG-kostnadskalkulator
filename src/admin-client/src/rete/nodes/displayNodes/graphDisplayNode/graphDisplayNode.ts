c8beb92b4d09d5c0d957f4cfe25206f6a71f67d9
andrrma <andrrma@stud.ntnu.no>
2024-04-21 20:34:10 +0200

    Adds toggling of inputs the user can select from. Adds distinct inputs with controllers for each result grouping the user can choose from.

--- src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNode.ts
+++ src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNode.ts
@@ -2,32 +2,35 @@ import {ParseableBaseNode} from "../../parseableBaseNode";
 import {ResultSocket} from "../../../sockets";
 import {NodeControl} from "../../nodeControl";
 import {NodeType} from "@skogkalk/common/dist/src/parseTree";
-import {ClassicPreset} from "rete";
-import {GraphDisplayNodeControlData} from "./graphDisplayNodeControlData";
-import {GraphDisplayNodeControlContainer} from "./graphDisplayNodeControlContainer";
+import {ClassicPreset, getUID} from "rete";
+import {GraphDisplayGroupData, GraphDisplayNodeControlData} from "./graphDisplayNodeControlData";
+import {GraphDisplayGroupControlContainer, GraphDisplayNodeControlContainer} from "./graphDisplayNodeControlContainer";
 import {GraphDisplayNode as ParseGraphDisplayNode} from "@skogkalk/common/src/parseTree";
 
 
+interface SerializedControls {
+    mainControlData: GraphDisplayNodeControlData,
+    inputControlData: { key: string, data: GraphDisplayGroupData }[]
+}
+
+
 export class GraphDisplayNode extends ParseableBaseNode <
-    { input: ResultSocket },
+    Record<string, ResultSocket>,
     {},
     { c: NodeControl<GraphDisplayNodeControlData> }
 > {
     constructor(
         protected updateNodeRendering: (nodeID: string) => void,
         private updateStore: () => void,
+        private removeConnection: (nodeID: string, connection?:{input?:string, output?:string}) => void,
         id?: string,
     ) {
         super(NodeType.GraphDisplay, 600, 400, "Graph Display", id);
-        console.log("graph constructor id", this.id);
-
-        this.addInput("input",
-            new ClassicPreset.Input(
-                new ResultSocket(),
-                "Result",
-                true))
 
         const initialControlData: GraphDisplayNodeControlData = {
+            shouldAddGroup: false,
+            resultGrouping: [],
+            inputFieldShow: [],
             nodeID: this.id,
             name: "",
             unit: "",
@@ -37,7 +40,10 @@ export class GraphDisplayNode extends ParseableBaseNode <
             new NodeControl(
                 initialControlData,
                 {
-                    onUpdate: () => {
+                    onUpdate: (data: Partial<GraphDisplayNodeControlData>) => {
+                        if(data.shouldAddGroup) {
+                            data.shouldAddGroup = false;
+                        }
                         updateNodeRendering(this.id);
                         updateStore();
                     },
@@ -48,28 +54,110 @@ export class GraphDisplayNode extends ParseableBaseNode <
         );
     }
 
-    data( inputs :{ input?: {name: string, value: number, id: string , color: string}[] }) : {} {
-        const { input } = inputs
-        if(input) {
-            this.controls.c.setNoUpdate({inputs: input.map((node, index)=>{return { label: node.name, id: node.id, value: node.value, color: node.color, ordering: index}})});
+    data( inputs : Record<string, {name: string, value: number, id: string , color: string}[]>) : {} {
+        const groupIDs = Object.keys(inputs);
+        for(const groupID of groupIDs) {
+
+            const results = inputs[groupID];
+            const resultIDs = results.map(result=>result.id);
+            let oldResults = [...this.controls.c.get('inputs')]
+                .filter(inputNode=>!resultIDs.includes(inputNode.id));
+            const inputData = results.map((node, index)=>{
+                return { label: node.name, id: node.id, value: node.value, color: node.color, ordering: index}
+            });
+            inputData.push(...oldResults);
+
+            const inputGroups = [...this.controls.c.get('resultGrouping')];
+            const inputGroup = inputGroups.find(group=>group.id === groupID);
+            if(!inputGroup) { throw new Error("Invalid input group found in control c of DisplayGraph node")}
+            inputGroup.inputIDs = resultIDs;
+
+            this.controls.c.setNoUpdate({inputs: inputData, resultGrouping: inputGroups});
+
             this.updateStore();
         }
+
         this.updateNodeRendering?.(this.id);
         return {}
     }
 
-    serializeControls(): any {
-        return this.controls.c.getData();
+
+    addInputGroup(inputID?: string) {
+        if(!inputID) inputID = getUID();
+        this.addInput(inputID, new ClassicPreset.Input(new ResultSocket(), "Result", true));
+        const control = new NodeControl<GraphDisplayGroupData>(
+            { id: inputID, name: "", shouldDelete: false, unit: "", label: "" },
+            {
+                onUpdate: (data: Partial<GraphDisplayGroupData>) => {
+                    if(data.shouldDelete) {
+                        this.removeInput(inputID!);
+                        this.removeConnection(this.id, {input: inputID});
+                        const resultGroups = [...this.controls.c.get('resultGrouping')]
+                            .filter(group=>{
+                                return (this.inputs[inputID!]?.control as NodeControl<GraphDisplayGroupData> | undefined)?.get('id') === group.id
+                            });
+
+                        this.controls.c.setNoUpdate({resultGrouping: resultGroups})
+                    } else {
+                        const oldGrouping = this.controls.c.get('resultGrouping');
+                        const resultGroups = [...oldGrouping].filter(group=>group.id !== inputID);
+                        const thisGroup = oldGrouping.find(group=>group.id === inputID);
+                        if(thisGroup === undefined) throw new Error("Graph result group not in resultGrouping of main control");
+                        thisGroup.name = data.name ?? thisGroup.name;
+                        thisGroup.unit = data.unit ?? thisGroup.unit;
+                        thisGroup.label = data.label ?? thisGroup.label;
+
+                        resultGroups.push(thisGroup);
+                    }
+
+                    this.updateNodeRendering(this.id);
+                    this.updateStore();
+                },
+                minimized: false,
+            },
+            GraphDisplayGroupControlContainer
+        )
+        this.inputs[inputID]?.addControl(control);
+        const currentGroups = [...this.controls.c.get('resultGrouping')];
+        currentGroups.push({
+            id: control.get('id'),
+            name: control.get('name'),
+            inputIDs: [],
+            unit: control.get('unit'),
+            label: control.get('label')
+        })
+        this.controls.c.set({resultGrouping: currentGroups})
+
     }
 
-    deserializeControls(data: any) {
-        this.controls.c.set(data);
+    serializeControls(): SerializedControls {
+        const inputKeys = Object.keys(this.inputs);
+        let inputControls = [];
+        for(const key of inputKeys) {
+            inputControls.push({ key: key, data: (this.inputs[key]?.control as NodeControl<GraphDisplayGroupData>).getData()});
+        }
+        return {
+            mainControlData: this.controls.c.getData(),
+            inputControlData: inputControls
+        }
+    }
+
+    deserializeControls(data: SerializedControls) {
+        data.inputControlData.forEach(({key, data})=>{
+            this.addInputGroup(key);
+            (this.inputs[key]?.control as NodeControl<GraphDisplayGroupData>).setNoUpdate(data);
+        })
+        this.controls.c.set(data.mainControlData);
+
+
     }
 
     toParseNode() : ParseGraphDisplayNode {
-        console.log("graphid", this.id);
         this.controls.c.setNoUpdate({nodeID: this.id})
+        console.log("resultGrouping", this.controls.c.get('resultGrouping'))
         return {
+            resultGroups: [...this.controls.c.get('resultGrouping')],
+            displayedInputIDs: this.controls.c.get('inputFieldShow').filter(field=>field.show).map(field=>field.id),
             unit: this.controls.c.get('unit'),
             name: this.controls.c.get('name'),
             inputs: [],
