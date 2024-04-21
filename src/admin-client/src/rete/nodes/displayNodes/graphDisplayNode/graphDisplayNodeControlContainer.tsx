2c1659851a7d6f00be893c039834a6959b96c889
andrrma <andrrma@stud.ntnu.no>
2024-04-21 20:34:18 +0200

    Adds toggling of inputs the user can select from. Adds distinct inputs with controllers for each result grouping the user can choose from.

--- src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNodeControlContainer.tsx
+++ src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNodeControlContainer.tsx
@@ -1,5 +1,5 @@
 import {NodeControl} from "../../nodeControl";
-import {GraphDisplayNodeControlData} from "./graphDisplayNodeControlData";
+import {GraphDisplayGroupData, GraphDisplayNodeControlData} from "./graphDisplayNodeControlData";
 import {ResultGraph} from "@skogkalk/common/dist/src/visual/ResultGraph";
 import {getNodeByID} from "@skogkalk/common/dist/src/parseTree";
 import {useAppSelector} from "../../../../state/hooks";
@@ -8,6 +8,33 @@ import {GraphDisplayNode as ParseGraphDisplayNode} from "@skogkalk/common/src/pa
 import {Provider} from "react-redux";
 import {Drag} from "rete-react-plugin";
 import {useEffect} from "react";
+import {Accordion, Button, Form, FormGroup} from "react-bootstrap";
+import {TextInputField} from "../../../../components/input/textInputField";
+
+
+export function GraphDisplayGroupControlContainer(
+    props: { data: NodeControl<GraphDisplayGroupData> }
+) {
+    return <>
+        <Drag.NoDrag>
+            <FormGroup>
+                <TextInputField value={props.data.get('name')} onChange={(value)=>{
+                    props.data.set({name: value})
+
+                }}
+                    inputHint={"Group name"}
+                />
+                <Button
+                    onClick={()=>{
+                        props.data.set({shouldDelete: true})
+                    }}
+                >Delete</Button>
+            </FormGroup>
+        </Drag.NoDrag>
+    </>
+}
+
+
 
 
 export function GraphDisplayNodeControlContainer(
@@ -25,21 +52,66 @@ function GraphDisplayNodeControlContainerContent(
     props: { data: NodeControl<GraphDisplayNodeControlData> }
 ) {
     const treeState = useAppSelector(selectTreeState);
+    const inputFieldShow = props.data.get('inputFieldShow');
     const nodeData = getNodeByID(treeState.tree, props.data.get('nodeID')) as ParseGraphDisplayNode;
     const displayArrangements = useAppSelector(selectDisplayArrangements)
+    const inputFields = treeState.tree?.inputs;
 
 
     useEffect(() => {
         props.data.set({arrangement: displayArrangements[props.data.get('nodeID')]})
     }, [displayArrangements, props.data]);
 
-    console.log("nodeData", nodeData, props.data.get('nodeID'), treeState.tree);
+    useEffect(()=>{
+        const inputs = inputFields?.map((input)=>{
+            return {
+                name: input.name,
+                page: input.pageName,
+                id: input.id,
+                show: false,
+            };
+
+        })
+        if(!inputs) { return }
+        const displayNodeInputIDs = [...inputFieldShow];
+
+        for(const input of inputs) {
+            const show = displayNodeInputIDs.find(data=>data.id===input.id)?.show;
+            input.show = show ?? true;
+        }
+        props.data.setNoUpdate({inputFieldShow: inputs});
+    }, [inputFieldShow, inputFields, props.data])
+
+
+
+
     return<>
+
         {( treeState.tree && nodeData) &&  <ResultGraph
             treeState={treeState.tree}
             displayData={nodeData}
         /> }
-
+        <Accordion defaultActiveKey="-1">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Toggle available inputs</Accordion.Header>
+                <Accordion.Body>
+                    {inputFieldShow.map((input, index)=>{
+                        return <Form.Check
+                            label={`${input.page} : ${input.name}`}
+                            checked={input.show}
+                            onChange={(e)=>{
+                                const newFieldShow = [...inputFieldShow];
+                                newFieldShow[index].show = e.currentTarget.checked;
+                                props.data.set({inputFieldShow: newFieldShow})
+                            }}
+                        ></Form.Check>
+                    })}
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+        <Button onClick={()=>{
+            props.data.set({shouldAddGroup: true})
+        }}>Add result group</Button>
     </>
 }
 
