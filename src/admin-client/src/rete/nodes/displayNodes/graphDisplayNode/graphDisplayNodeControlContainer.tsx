d9e91eaf9b02db0ba64964e2cedb9c4917ee4c59
andrrma <andrrma@stud.ntnu.no>
2024-04-22 13:00:53 +0200

    Adds dropdown for selecting unit of result group

--- src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNodeControlContainer.tsx
+++ src/admin-client/src/rete/nodes/displayNodes/graphDisplayNode/graphDisplayNodeControlContainer.tsx
@@ -3,33 +3,76 @@ import {GraphDisplayGroupData, GraphDisplayNodeControlData} from "./graphDisplay
 import {ResultGraph} from "@skogkalk/common/dist/src/visual/ResultGraph";
 import {getNodeByID} from "@skogkalk/common/dist/src/parseTree";
 import {useAppSelector} from "../../../../state/hooks";
-import {selectDisplayArrangements, selectTreeState, store} from "../../../../state/store";
+import {selectDisplayArrangements, selectTreeState, selectUnits, store} from "../../../../state/store";
 import {GraphDisplayNode as ParseGraphDisplayNode} from "@skogkalk/common/src/parseTree"
 import {Provider} from "react-redux";
 import {Drag} from "rete-react-plugin";
-import {useEffect} from "react";
-import {Accordion, Button, Form, FormGroup} from "react-bootstrap";
+import React, {useEffect} from "react";
+import {Accordion, Button, Col, FloatingLabel, Form, FormSelect, Row} from "react-bootstrap";
 import {TextInputField} from "../../../../components/input/textInputField";
+import Container from "react-bootstrap/Container";
+import parse from "html-react-parser";
 
 
 export function GraphDisplayGroupControlContainer(
     props: { data: NodeControl<GraphDisplayGroupData> }
 ) {
+    return <Provider store={store}>
+        <GraphDisplayGroupControlContents data={props.data}/>
+    </Provider>
+}
+
+export function GraphDisplayGroupControlContents(
+    props : { data: NodeControl<GraphDisplayGroupData> }
+) {
+    const units = useAppSelector(selectUnits);
+
+
     return <>
         <Drag.NoDrag>
-            <FormGroup>
-                <TextInputField value={props.data.get('name')} onChange={(value)=>{
-                    props.data.set({name: value})
+            <Container>
+                <Row >
+                    <Col xs={6}>
+                        <TextInputField
+                            value={props.data.get('name')}
+                            onChange={(value)=>{
+                                props.data.set({name: value})
+                            }}
+                            inputHint={"Name"}
+                        />
+                    </Col>
+                    <Col xs={4}>
+                        <FloatingLabel
+                            label={"Unit"}
+                            onPointerDown={(e)=>{e.stopPropagation()}}
+                            onDoubleClick={e=>{e.stopPropagation()}}
+                        >
+                            <FormSelect
+                                className={"field"}
+                                aria-label="Default select example"
+                                value={props.data.getData().unit}
+                                onChange={(e)=> {
+                                    props.data.set({unit: e.currentTarget.value})
+                                }}
 
-                }}
-                    inputHint={"Group name"}
-                />
-                <Button
-                    onClick={()=>{
-                        props.data.set({shouldDelete: true})
-                    }}
-                >Delete</Button>
-            </FormGroup>
+                            >
+                                <option value={undefined} >...</option>
+                                {units.map(unit=>{
+                                    return <option key={unit.id} value={unit.unit.name}>{parse(unit.unit.name)}</option>
+                                })
+                                }
+                            </FormSelect>
+                        </FloatingLabel>
+                    </Col>
+                    <Col xs={2}>
+                        <Button
+                            onClick={()=>{
+                                props.data.set({shouldDelete: true})
+                            }}
+                        >Delete</Button>
+                    </Col>
+                </Row>
+            </Container>
         </Drag.NoDrag>
     </>
 }
